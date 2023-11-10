ef8f1a4767d80fdf981c427577fe208611d2466b
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-10 13:25:05 +0700

    define Provider and store

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import noteReducer from './reducers/noteReducer.js'
+
+const store = createStore(noteReducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
 )
