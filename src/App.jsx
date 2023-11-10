e67f4386801e438291f0155a39e11542fc15f65a
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-10 11:02:01 +0700

    initial app code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
+import { createStore } from 'redux'
 import './App.css'
 
-function App() {
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    state.push(action.payload)
+  }
+
+  return state
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+const App = () => {
 
   return (
+    <div>
+      <ul>
+        {store.getState().map(note => (
+          <li key={note.id}>
+            {note.content} | 
+            <strong>
+              {note.important ? 'important' : 'not important'}
+            </strong>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
