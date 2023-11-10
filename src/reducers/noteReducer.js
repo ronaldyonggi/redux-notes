b9f13121e3f60712981eaa85c22d56472841260b
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-10 13:27:54 +0700

    action creators added

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -25,4 +25,22 @@ const noteReducer = (state = [], action) => {
 
 const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 
+const createNote = content => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = id => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
+  }
+}
+
 export default noteReducer
