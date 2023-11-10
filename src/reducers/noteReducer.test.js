07c566029ce9881dd9443b60c51a7c3e4d99badc
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-10 12:33:19 +0700

    add test for toggle importance of notes

--- src/reducers/noteReducer.test.js
+++ src/reducers/noteReducer.test.js
@@ -19,4 +19,41 @@ describe ('noteReducer', () => {
         expect(newState).toHaveLength(1)
         expect(newState).toContainEqual(action.payload)
     })
+
+    test('returns new state with action TOGGLE_IMPORTANCE', () => {
+        const state = [
+            {
+                content: 'the app state is in redux store',
+                important: true,
+                id: 1
+            },
+            {
+                content: 'state changes are made with actions',
+                important: false,
+                id: 2
+            }
+        ]
+
+        const action = {
+            type: 'TOGGLE_IMPORTANCE',
+            payload: {
+                id: 2
+            }
+        }
+
+        deepFreeze(state)
+
+        const newState = noteReducer(state, action)
+        expect(newState).toHaveLength(2)
+        // Ensures the first note is unchanged
+        expect(newState).toContainEqual(state[0])
+
+        // Ensures the second note's important is now true
+        expect(newState).toContainEqual({
+            content: 'state changes are made with actions',
+            important: true,
+            id: 2
+        })
+
+    })
 })
