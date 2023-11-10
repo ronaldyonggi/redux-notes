eb37d3d1130cd6f7f2b1c69fadeecce1f99c0c32
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-10 11:17:23 +0700

    noteReducer moved to a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
 import { createStore } from 'redux'
 import './App.css'
 
-const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    return state.concat(action.payload)
-  }
-
-  return state
-}
-
 const store = createStore(noteReducer)
 
 store.dispatch({
