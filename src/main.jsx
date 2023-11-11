9c20f6e695f683551d2017b1cdb83482cd149744
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-11 15:32:02 +0700

    combine reducers

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
 import noteReducer from './reducers/noteReducer.jsx'
+import filterReducer from './reducers/filterReducer.jsx'
 
-const store = createStore(noteReducer)
+import { createNote } from './reducers/noteReducer.jsx'
+import { filterChange } from './reducers/filterReducer.jsx'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer)
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    {/* <App /> */}
+    <div />
   </Provider>
 )
