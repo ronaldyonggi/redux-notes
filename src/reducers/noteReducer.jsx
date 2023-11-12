49d368670483b6d38cb2a2d4dfdda186c404c8da
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-12 17:58:22 +0700

    reducer now uses createSlice from RTK

--- src/reducers/noteReducer.jsx
+++ src/reducers/noteReducer.jsx
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit"
+
 const initialState = [
   {
     content: 'reducer defines how redux store works',
@@ -11,49 +13,78 @@ const initialState = [
   }
 ]
 
-const noteReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case 'NEW_NOTE':
-            // return state.concat(action.payload)
-            return [...state, action.payload]
-        case 'TOGGLE_IMPORTANCE': {
-            // Get id from action's payload
-            const id = action.payload.id 
-            // Find the note that needs to be changed from state
-            const noteToChange = state.find(note => note.id === id)
-            
-            // Create a whole new updated note
-            const updatedNote = {
-                ...noteToChange,
-                important: !noteToChange.important
-            }
-
-            // Update state
-            return state.map(note => note.id === id ? updatedNote : note)
-        }
-        default:
-            return state
-    }
-}
-
 const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 
-export const createNote = content => {
-  return {
-    type: 'NEW_NOTE',
-    payload: {
-      content,
-      important: false,
-      id: generateId()
+// const noteReducer = (state = initialState, action) => {
+//     switch(action.type) {
+//         case 'NEW_NOTE':
+//             // return state.concat(action.payload)
+//             return [...state, action.payload]
+//         case 'TOGGLE_IMPORTANCE': {
+//             // Get id from action's payload
+//             const id = action.payload.id 
+//             // Find the note that needs to be changed from state
+//             const noteToChange = state.find(note => note.id === id)
+            
+//             // Create a whole new updated note
+//             const updatedNote = {
+//                 ...noteToChange,
+//                 important: !noteToChange.important
+//             }
+
+//             // Update state
+//             return state.map(note => note.id === id ? updatedNote : note)
+//         }
+//         default:
+//             return state
+//     }
+// }
+
+
+// export const createNote = content => {
+//   return {
+//     type: 'NEW_NOTE',
+//     payload: {
+//       content,
+//       important: false,
+//       id: generateId()
+//     }
+//   }
+// }
+
+// export const toggleImportanceOf = id => {
+//   return {
+//     type: 'TOGGLE_IMPORTANCE',
+//     payload: { id }
+//   }
+// }
+
+const noteSlice = createSlice({
+  name: 'notes',
+  initialState,
+  reducers: {
+    createNote(state, action) {
+      const content = action.payload
+      state.push({
+        content,
+        important: false,
+        id: generateId()
+      })
+    },
+    toggleImportanceOf(state, action) {
+      const id = action.payload
+      // Find the note whose importance's about to be changed (matching id)
+      const noteToChange = state.find(n => n.id === id)
+      // Create the updated note
+      const updatedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      }
+
+      return state.map(note => note.id === id ? updatedNote : note)
     }
   }
-}
+})
 
-export const toggleImportanceOf = id => {
-  return {
-    type: 'TOGGLE_IMPORTANCE',
-    payload: { id }
-  }
-}
-
-export default noteReducer
+export const { createNote, toggleImportanceOf } = noteSlice.actions
+export default noteSlice.reducer
