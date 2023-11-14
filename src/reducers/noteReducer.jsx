947d0409a34300bcce41a2c07b428b90acd28a8b
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-14 14:39:40 +0700

    noteReducer initially has no notes

--- src/reducers/noteReducer.jsx
+++ src/reducers/noteReducer.jsx
@@ -1,18 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2
-  }
-]
-
 const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 
 // const noteReducer = (state = initialState, action) => {
@@ -61,7 +48,7 @@ const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 
 const noteSlice = createSlice({
   name: 'notes',
-  initialState,
+  initialState: [],
   reducers: {
     createNote(state, action) {
       const content = action.payload
