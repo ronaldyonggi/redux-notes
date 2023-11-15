ca9732da7db24296fcab8aaf532e2757a5aefb66
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-15 17:31:10 +0700

    cleaned up commented out parts

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,11 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 
-// import noteService from './services/notes.jsx'
 import noteReducer from './reducers/noteReducer.jsx'
 import filterReducer from './reducers/filterReducer.jsx'
 
 import { createNote } from './reducers/noteReducer.jsx'
 import { filterChange } from './reducers/filterReducer.jsx'
-
-// const reducer = combineReducers({
-//   notes: noteReducer,
-//   filter: filterReducer
-// })
-
-// const store = createStore(reducer)
 
 const store = configureStore({
   reducer: {
@@ -26,10 +18,6 @@ const store = configureStore({
     filter: filterReducer
   }
 })
-
-// store.subscribe(() => console.log(store.getState()))
-// store.dispatch(filterChange('IMPORTANT'))
-// store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
