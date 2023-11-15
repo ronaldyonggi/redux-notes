56d1c815fc44c842d74cef67903965c6a483b728
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-15 17:32:56 +0700

    store moved to separate file

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
 
-import noteReducer from './reducers/noteReducer.jsx'
-import filterReducer from './reducers/filterReducer.jsx'
-
-import { createNote } from './reducers/noteReducer.jsx'
-import { filterChange } from './reducers/filterReducer.jsx'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
+import store from '../store.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
