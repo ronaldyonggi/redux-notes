04b7ae1a6e116f9f04d4a975efec982debe0afa0
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-15 11:06:19 +0700

    cleaned up commented out parts

--- src/reducers/noteReducer.jsx
+++ src/reducers/noteReducer.jsx
@@ -2,61 +2,11 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 
-// const noteReducer = (state = initialState, action) => {
-//     switch(action.type) {
-//         case 'NEW_NOTE':
-//             // return state.concat(action.payload)
-//             return [...state, action.payload]
-//         case 'TOGGLE_IMPORTANCE': {
-//             // Get id from action's payload
-//             const id = action.payload.id 
-//             // Find the note that needs to be changed from state
-//             const noteToChange = state.find(note => note.id === id)
-            
-//             // Create a whole new updated note
-//             const updatedNote = {
-//                 ...noteToChange,
-//                 important: !noteToChange.important
-//             }
-
-//             // Update state
-//             return state.map(note => note.id === id ? updatedNote : note)
-//         }
-//         default:
-//             return state
-//     }
-// }
-
-
-// export const createNote = content => {
-//   return {
-//     type: 'NEW_NOTE',
-//     payload: {
-//       content,
-//       important: false,
-//       id: generateId()
-//     }
-//   }
-// }
-
-// export const toggleImportanceOf = id => {
-//   return {
-//     type: 'TOGGLE_IMPORTANCE',
-//     payload: { id }
-//   }
-// }
-
 const noteSlice = createSlice({
   name: 'notes',
   initialState: [],
   reducers: {
     createNote(state, action) {
-      // const content = action.payload
-      // state.push({
-      //   content,
-      //   important: false,
-      //   id: generateId()
-      // })
       state.push(action.payload)
 
     },
